97ecb7187d9da8acff3398cd09658b64b20bfeef
dcastro18 <h.davidcastro14@gmail.com>
2021-11-02 18:57:50 -0600

    Checker v2

--- Codigo/ide-triangle-v1.1.src/src/Triangle/IDECompiler.java
+++ Codigo/ide-triangle-v1.1.src/src/Triangle/IDECompiler.java
@@ -64,9 +64,9 @@ public class IDECompiler {
         writerHTML.generateHTML(filesDestination);
         if (report.numErrors == 0) {
 
-            //System.out.println("Contextual Analysis ...");
-            //Checker checker = new Checker(report);
-            //checker.check(rootAST);
+            System.out.println("Contextual Analysis ...");
+            Checker checker = new Checker(report);
+            checker.check(rootAST);
             if (report.numErrors == 0) {
                 //System.out.println("Code Generation ...");
                 //Encoder encoder = new Encoder(report);
